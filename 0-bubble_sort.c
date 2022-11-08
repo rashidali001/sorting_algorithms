efd8a60bbaf957cd63fa6282038c2e29fd2a248d
rashidali001 <rashidestonian@gmail.com>
2022-11-08 14:40:12 +0300

    Bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ * bubble_sort - sorts the elements in a list
+ *
+ * @array: list to be received
+ * @size: number of elements in the list
+ */
+void bubble_sort(int *array, size_t size)
+{
+    unsigned int temp, loop_operation, loop_array;
+
+    for (loop_operation = 1; loop_operation <= size; loop_operation++)
+    {
+        bool swapped = false;
+        for (loop_array = 0; loop_array < (size - loop_operation); loop_array++)
+        {
+            if (array[loop_array] > array[loop_array + 1])
+            {
+                temp = array[loop_array];
+                array[loop_array] = array[loop_array + 1];
+                array[loop_array + 1] = temp;
+                swapped = true;
+                print_array(array, size);
+            }
+        }
+        if (!swapped)
+        {
+            break;
+        }
+    }
+}
---
+++ sort.h
@@ -0,0 +1,48 @@
+#ifndef SORT_H
+
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
+/**
+ * print_array - Prints an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+
+void print_array(const int *array, size_t size);
+
+/**
+ * print_list - Prints a list of integers
+ *
+ * @list: The list to be printed
+ */
+void print_list(const listint_t *list);
+
+
+/**
+ * bubble_sort - Entry point
+ *
+ * Return: None
+ */
+void bubble_sort(int *array, size_t size);
+
+#endif
